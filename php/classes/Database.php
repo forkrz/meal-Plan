08cb43cc7b7e3d931372c5cfebc5eeaee44819d9
forkrz <forkrz8@gmail.com>
2021-11-01 21:52:31 +0100

    connection with db established, function to get Data from DB added

---
+++ php/classes/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+class Database
+{
+    function __construct(array $config)
+    {
+        try {
+            $dsn = "mysql:host={$config['host']};dbname={$config['dbname']}";
+            $this->con = new PDO($dsn, $config['user'], $config['password']);
+            $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
+
+    public function getData(string $login)
+    {
+        $query = "SELECT * FROM users WHERE USER_LOGIN =:USER_LOGIN";
+        $statement = $this->con->prepare($query);
+        $statement->bindParam(':USER_LOGIN', $login);
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        print_r($result);
+        return $result;
+    }
+}
---
+++ php/login.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+require_once "../php/classes/Database.php";
+$config = require_once './config/config.php';
+$data = new Database($config);
+
+$data->getData('test');
