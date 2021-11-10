ea875b527fe2103c75773a14ede3be70b687aad4
forkrz <forkrz8@gmail.com>
2021-11-10 23:49:58 +0100

    getRandomRecipe method added

--- php/classes/MealPlans.php
+++ php/classes/MealPlans.php
@@ -2,11 +2,17 @@
 
 declare(strict_types=1);
 
-class MealPLans
+class Meals
 {
     public function generateMealPlan(string $timeFrame, string $targetCalories, string $diet)
     {
         $url = "https://api.spoonacular.com/mealplanner/generate?apiKey=405902d98d8745d5a055a828ea0d7330&timeFrame={$timeFrame}&targetCalories={$targetCalories}&diet={$diet}.";
+        return $url;
+    }
+
+    public function getRandomRecipe(string $quantity, string $tags)
+    {
+        $url = "https://api.spoonacular.com/recipes/random?apiKey=405902d98d8745d5a055a828ea0d7330&number={$quantity}&tags={$tags}&limitLicense=true.";
         return $url;
     }
 }
--- php/getMealPlan.php
+++ php/getMealPlan.php
@@ -9,6 +9,6 @@ header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$mealPlans = new MealPLans();
-$response = file_get_contents($mealPlans->generateMealPlan('day', '3000', 'vegetarian'));
+$mealPlans = new Meals();
+$response = file_get_contents($mealPlans->getRandomRecipe("50", "vegetarian,dessert"));
 var_dump($response);
