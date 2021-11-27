43d44f534d2fb989bff147926fba88959e9a5dc1
forkrz <forkrz8@gmail.com>
2021-11-27 23:58:02 +0100

    display random recipes test modal modified

--- js/test.js
+++ js/test.js
@@ -4,6 +4,13 @@ let i = 0;
 const modalContent = document.getElementById('modalContent');
 const modal = document.getElementById('modal');
 
+
+meals.forEach(meal => {
+    console.log(meal.extendedIngredients);
+})
+
+
+
 function addList() {
     modalContent.insertAdjacentHTML('beforeend',
         `<button class="material-icons modal__header__button" id="closeButton">close</button>
@@ -14,14 +21,24 @@ function addList() {
 
 function testt() {
     const list = document.getElementById('randomRecpiesList');
-    list.insertAdjacentHTML('beforeend', '<li class="randomRecpiesList__element">' + meals[i].title + '</li>' +
-        '<a href="#" class="randomRecpiesList__href">Show recipe</a>' + '<a href="#" class="randomRecpiesList__href">Save</a>');
+    if (meals[i].title.length >= 10) {
+        list.insertAdjacentHTML('beforeend', '<li class="randomRecpiesList__element">' + '<span class="randomRecpiesList__element__span">' +
+            meals[i].title.substring(0, 12) + '...' + "</span>" +
+            '<a href="#" class="randomRecpiesList__href">Show recipe</a>' + '<a href="#" class="randomRecpiesList__href" id="showMeal">Save</a>' + '</li>');
+    } else {
+        list.insertAdjacentHTML('beforeend', '<li class="randomRecpiesList__element">' + meals[i] + '</li>' +
+            '<a href="#" class="randomRecpiesList__href">Show recipe</a>' + '<a href="#" class="randomRecpiesList__href">Save</a>');
+    }
+
     i++;
 }
+const igridients = meals[i].extendedIngredients;
+
 
 function showMeals() {
     addList();
     meals.forEach(testt);
+    modalContent.insertAdjacentHTML('beforeend', `<span class="material-icons getNewRandomRecpies">autorenew</span>`)
 }
 
 showMeals();
